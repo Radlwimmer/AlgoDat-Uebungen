0323b7cbf37ced4535ea1a0e1df8f5aa723aace5
Irene Radlwimmer <i_radlwimmer@hotmail.com>
2021-05-20 18:54:56 +0200

    exercise 2

--- uebungsblatt6.py
+++ uebungsblatt6.py
@@ -1,4 +1,4 @@
-#---exercize 1---
+#---exercise 1---
 
 def binary_search(numbers:list,num:int):
     # find the middle index (cast as int)
@@ -23,6 +23,22 @@ def binary_search(numbers:list,num:int):
 
 ordered_list = [3,5,6,8,10,11,11,13,16]
 x = 6
-print(binary_search(ordered_list,x))
+#print(binary_search(ordered_list,x))
 
+#---excercise 2---
 
+def bubble_sort(numbers:list):
+    bool = True
+    while bool:
+        bool = False
+        for i, x in enumerate(numbers):
+            if len(numbers) == i+1:
+                break
+            elif x < numbers[i+1]:
+                numbers[i],numbers[i+1] = numbers[i+1],numbers[i]
+                bool = True
+    return numbers
+
+unordered_list = [8,4,6,10,3,4,7]
+
+print(bubble_sort(unordered_list))
