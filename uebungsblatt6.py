41b49d1fe8f45c02f9b971503e4089ea895bd0f7
Irene Radlwimmer <i_radlwimmer@hotmail.com>
2021-05-20 18:11:01 +0200

    fixed binary search bugs

--- uebungsblatt6.py
+++ uebungsblatt6.py
@@ -1,29 +1,28 @@
 #---exercize 1---
 
 def binary_search(numbers:list,num:int):
-    # find the middle index (cast as int, rounded up)
-    middle = int((len(numbers)+1)/2)
-    # if num is exactly at middle, return middle
+    # find the middle index (cast as int)
+    middle = int((len(numbers))/2)
+    # if num is exactly at middle, return middle (this also counts if the list only has one value)
     if numbers[middle] == num:
-        return true
+        return True
+    # if list has only one value and that's not "num" return false
+    elif len(numbers) == 1:
+        return False
     # if num is smaller than the value at middle, check below
     elif numbers[middle] > num:
         # define x as the part of the list to search through
-        x = slice(0,middle-1)
+        x = slice(0,middle)
         return binary_search(numbers[x],num)
     # if num is bigger than the value at middle, check above
     elif numbers[middle] < num:
         #define x as the part of the list to search through
-        x = slice(middle, len(numbers)-1)
+        x = slice(middle, len(numbers))
         return binary_search(numbers[x],num)
-    else:
-        print(f"{num} is not in the list.")
-        return -1
+
 
 ordered_list = [3,5,6,8,10,11,11,13,16]
-x = 8
+x = 6
 print(binary_search(ordered_list,x))
-middle = int((len(ordered_list)+1)/2)
-x = slice(middle,len(ordered_list))
-print(ordered_list[x])
+
 
