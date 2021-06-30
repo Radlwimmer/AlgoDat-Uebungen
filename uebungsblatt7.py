83d0ea598043fc9a7fdac1a8dfb7e0b8ed974cc5
Irene Radlwimmer <i_radlwimmer@hotmail.com>
2021-06-30 12:50:37 +0200

    excercise complete with comments

--- uebungsblatt7.py
+++ uebungsblatt7.py
@@ -1,3 +1,37 @@
 #---exercise 1---
 
 class MyQueue:
+    #constructor starts out with empty list
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, element):
+        # append new element to END of list
+        self.queue.append(element)
+
+    def dequeue(self):
+        # only touch list if there are still elements
+        if len(self.queue) == 0:
+            print("Your queue is already empty")
+            return
+        else:
+            # remove FIRST element
+            self.queue.pop(0)
+
+    #override str method
+    def __str__(self):
+        print("List from first input to last input:")
+        # return list as string
+        return f"{self.queue}"
+
+Queue = MyQueue()
+Queue.enqueue("hello")
+Queue.enqueue("how")
+Queue.enqueue("are")
+Queue.enqueue("you")
+Queue.enqueue("doing")
+print(Queue)
+Queue.dequeue()
+print(Queue)
+
+#---exercise 2---
